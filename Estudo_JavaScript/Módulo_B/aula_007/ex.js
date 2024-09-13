e84d42ed0f06c30a7ca93723367f753721f5bd05
LaLunaInSky <moonaflordapele@gmail.com>
2024-09-13 15:12:44 -0300

    13/09/2024

--- Estudo_JavaScript/Módulo_B/aula_007/ex.js
+++ Estudo_JavaScript/Módulo_B/aula_007/ex.js
@@ -9,10 +9,30 @@ Operadores:
 
 /*
 Aritiméticos:
-'+'   //
-'-'   //
-'*'   //
-'/'   //
-'%'   //
-'**'  //
+binários - precisa de dois parâmetros para funcionar
+'+'   // 5 + 2 -> 7
+'-'   // 5 - 2 -> 3
+'*'   // 5 * 2 -> 10
+'/'   // 5 / 2 -> 2.5
+'%'   // 5 % 2 -> 1
+'**'  // 5 ** 2 -> 25
 */
+
+var a = 5 + 3
+var b = a % 5
+var c = 5 * b ** 2
+var d = 10 - a / 2
+var e = 6 * 2 / d
+var f = b % e + 4 / e
+
+var n = 3
+n += 4
+n -= 5
+n *= 4
+n /= 2
+n **= 2
+n %= 5
+n ++
+n ++
+n ++
+console.log(a, b, c, d, e, f, n)

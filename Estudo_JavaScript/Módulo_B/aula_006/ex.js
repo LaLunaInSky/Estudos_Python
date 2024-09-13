be5dcf2a7326c4fb30c4ee74ef5581b72f991ccb
LaLunaInSky <moonaflordapele@gmail.com>
2024-09-13 10:21:43 -0300

    13/09/2024 10:21

--- Estudo_JavaScript/Módulo_B/aula_006/ex.js
+++ Estudo_JavaScript/Módulo_B/aula_006/ex.js
@@ -1,6 +1,7 @@
 var num1 = Number(window.prompt("Digite um número"))
 var num2 = Number(window.prompt("Digite outro número"))
 
-//Number.parseInt(n) e Number.parseFloat(n) conversão para Inteiro e Real 
+//Number.parseInt(n) e Number.parseFloat(n) conversão para Inteiro e Real
+//String(n) ou n.toString() conversão para String
 
-window.alert('A soma dos números ' + num1 + ' e ' + num2 + ' é igual a ' + (num1 + num2) + '.')
+window.alert(`A soma dos números ${num1} e ${num2} é igual a ${num1 + num2}.`) // uso de template string

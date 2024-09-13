464e5e4e7f20e4c7bc502f2c75376eb817db198e
LaLunaInSky <moonaflordapele@gmail.com>
2024-09-13 10:32:16 -0300

    13/09/2024 10:32

--- Estudo_JavaScript/Módulo_B/aula_006/ex.js
+++ Estudo_JavaScript/Módulo_B/aula_006/ex.js
@@ -4,4 +4,21 @@ var num2 = Number(window.prompt("Digite outro número"))
 //Number.parseInt(n) e Number.parseFloat(n) conversão para Inteiro e Real
 //String(n) ou n.toString() conversão para String
 
-window.alert(`A soma dos números ${num1} e ${num2} é igual a ${num1 + num2}.`) // uso de template string
+var frase = `A soma dos números ${num1} e ${num2} é igual a ${num1 + num2}.` // uso de template string
+
+/*
+Formatando Strings:
+- n.length  //quantos caracteres a string tem
+- n.toUpperCase()   //transforma a string toda em MAIÚSCULA
+- n.toLowerCase()   //transfroma a string toda em minúscula
+*/
+
+window.alert(`
+    ${frase}
+
+    A frase tem ${frase.length} letras no total.
+    
+    ${frase.toUpperCase()}
+    
+    ${frase.toLowerCase()}
+`)

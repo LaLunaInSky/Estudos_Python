6e8e14505126420b07512187d98e2f3f67c6207d
LaLunaInSky <moonaflordapele@gmail.com>
2024-09-13 10:09:18 -0300

    13/09/2024 10:09

---
+++ Estudo_JavaScript/Módulo_B/aula_006/ex.js
@@ -0,0 +1,6 @@
+var num1 = Number(window.prompt("Digite um número"))
+var num2 = Number(window.prompt("Digite outro número"))
+
+//Number.parseInt(n) e Number.parseFloat(n) conversão para Inteiro e Real 
+
+window.alert('A soma dos números ' + num1 + ' e ' + num2 + ' é igual a ' + (num1 + num2) + '.')

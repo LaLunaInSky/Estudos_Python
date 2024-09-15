112813ff47791d22e8bc2cffc1532118d1f4dbd3
LaLunaInSky <moonaflordapele@gmail.com>
2024-09-15 11:22:00 -0300

    15/09/2024 11:21

--- Estudo_JavaScript/Módulo_B/aula_008/ex.js
+++ Estudo_JavaScript/Módulo_B/aula_008/ex.js
@@ -1,5 +1,6 @@
 /*
-relacionais:
+Relacionais:
+-resultado será valor boolean
 >   //maior
 <   //menor
 >=  //maior ou igual
@@ -7,6 +8,43 @@ relacionais:
 ==  //igual
 !=  //diferente
 
-lógicos
+Exemplos
+-não testa os types
+5 == 5 -> True
+5 != '5' -> False
+5 == '5' -> True
+
+5 > 2 -> True
+7 < 4 -> False
+8 >= 8 -> True
+9 <= 7 -> False
+5 == 5 -> True
+4 != 4 -> False
+
+Operador de Identidade
+-testa o type
+5 === '5' ->  False
+5 !== '5' -> True
+5 === 5 -> True
+-funciona com o !== e ===
+*/
+
+/*
+Lógicos:          No Python
+!   //negação       not
+&&  //conjunção     and
+||  //disjunção     or
+*/
+
+/*
+Ordem de precedêmcia
+- () ** / ...
+- > < >= ...
+- !
+- &&
+- ||
+*/
+
+/*
 ternário
 */

25feb22f579a45be3f5a54c1308a8d65f2ff77df
LaLunaInSky <moonaflordapele@gmail.com>
2025-05-07 23:40:27 -0300

    07/05/2025 23:40

--- Estudo_De_Volta_Ao_Python/Exercícios_Treinar/Exercícios_Curso_em_Vídeo/ex_077/main.py
+++ Estudo_De_Volta_Ao_Python/Exercícios_Treinar/Exercícios_Curso_em_Vídeo/ex_077/main.py
@@ -1,7 +1,41 @@
 from os import system
 
+def obterPalavrasDaTupla() -> tuple:
+    palavras: tuple = (
+        "aprender", "programar", "linguagem", "python", "curso", "gratis", "estudar", "praticar", "trabalhar", "mercado", "programador", "futuro"
+    )
+
+    return palavras
+
+
+def analisarAsVogaisDaPalavra(palavra: str) -> list:
+    vogais_da_palavra: list = []
+    
+    for letra in palavra:
+        if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
+            vogais_da_palavra.append(letra)
+
+    return vogais_da_palavra
+
+
+def mostrarInformaçõesDaPalavras(palavras_selecionadas: tuple) -> None:
+    for palavra in palavras_selecionadas:
+        print(f"A palavra {palavra.upper()} tem as vogais:", end="")
+
+        vogais_da_palavra: list = analisarAsVogaisDaPalavra(palavra)
+
+        for vogal in vogais_da_palavra:
+            print(f" {vogal}", end='')
+
+        print()
+
+
 def main() -> None:
     system("clear")
+
+    palavras_selecionadas: tuple = obterPalavrasDaTupla()
+
+    mostrarInformaçõesDaPalavras(palavras_selecionadas)
 
 
 main()
